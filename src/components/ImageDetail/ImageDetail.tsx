38cf53b0a2ba9df8eb3c7949f822e3f61f782bc1
King Larce Cortes <kingcort01@gmail.com>
2022-02-20 15:29:20 +0800

    feat: Add loading spinner for on load image

--- src/components/ImageDetail/ImageDetail.tsx
+++ src/components/ImageDetail/ImageDetail.tsx
@@ -1,16 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { ImageViewer } from "../ImageViewer/ImageViewer";
+import { LoadingSpinner } from "./style";
 
 interface ImageDetailProps {
   image: string;
   title: string;
 }
 
-export const ImageDetail: FC<ImageDetailProps> = (props) => (
-  <>
-    <ImageViewer>
-      <label>{props.title}</label>
-      <img src={props.image} alt="Your image" style={{ maxWidth: "100%" }} />
-    </ImageViewer>
-  </>
-);
+export const ImageDetail: FC<ImageDetailProps> = (props) => {
+  const [loading, setLoading] = useState(true);
+  const imageLoaded = () => setLoading(false);
+
+  return (
+    <div>
+      <div style={{ display: loading ? "block" : "none" }}>
+        <LoadingSpinner />
+      </div>
+      <div style={{ display: loading ? "none" : "block" }}>
+        <ImageViewer>
+          <label>{props.title}</label>
+          <img
+            key={props.image}
+            src={props.image}
+            alt={props.title}
+            style={{ maxWidth: "100%" }}
+            onLoad={imageLoaded}
+          />
+        </ImageViewer>
+      </div>
+    </div>
+  );
+};
